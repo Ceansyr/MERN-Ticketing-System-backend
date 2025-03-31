c912809eafb932b337cf01eb0d2aa71959a0a937
Ceansyr <chinmaymandavkar@gmail.com>
2025-03-31 19:13:56 +0530

    final commit

--- server.js
+++ server.js
@@ -18,39 +18,34 @@ const app = express();
 
 const corsOptions = {
   origin: [
-    "http://localhost:5173", // Local development
-    "https://astonishing-smakager-a8b308.netlify.app", // Frontend production URL
+    "http://localhost:5173",
+    "https://astonishing-smakager-a8b308.netlify.app",
   ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-  credentials: true, // Allow cookies and credentials
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
 
-// Apply CORS middleware
 app.use(cors(corsOptions));
 
-// Handle preflight requests
 app.options("*", cors(corsOptions));
 
-// Custom middleware for setting headers
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://astonishing-smakager-a8b308.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") {
-    return res.sendStatus(204); // Preflight request
+    return res.sendStatus(204);
   }
   next();
 });
 
-// Other middlewares
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(log);
 
-// Mount routes
 app.use("/api/user", userRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/availability", availabilityRoutes);
@@ -75,10 +70,8 @@ const startServer = async () => {
 
 startServer();
 
-// Error handler middleware
 app.use(errorHandler);
 
-// Default route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
