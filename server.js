1dc033ba172e16c698378616b2db8eec6864cd93
Ceansyr <chinmaymandavkar@gmail.com>
2025-03-31 12:35:17 +0530

    cores issue 3

--- server.js
+++ server.js
@@ -18,10 +18,20 @@ const app = express();
 
 const corsOptions = {
   origin: ["http://localhost:5000", "https://astonishing-smakager-a8b308.netlify.app/"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, 
 };
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(204); 
+});
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
