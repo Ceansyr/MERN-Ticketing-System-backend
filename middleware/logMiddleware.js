6c6b8a3e661f0f06350322642667130a38bfc198
Ceansyr <chinmaymandavkar@gmail.com>
2025-03-31 18:42:05 +0530

    updated logging middleware

--- middleware/logMiddleware.js
+++ middleware/logMiddleware.js
@@ -1,11 +1,24 @@
 import fs from "fs";
+import path from "path";
+
+const logDirectory = path.join("../logs");
+
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory, { recursive: true });
+}
+
 
 export default function log(req, res, next) {
   const date = new Date();
   const logFile = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}.log`;
-  fs.appendFile(`./logs/${logFile}`, `${req.method} ${req.url} ${req.ip}\n`, (err) => { 
+  const logPath = path.join(logDirectory, logFile);
+
+  const logEntry = `${date.toISOString()} ${req.method} ${req.url} ${req.ip}\n`;
+
+  fs.appendFile(logPath, logEntry, (err) => {
     if (err) throw err;
   });
+
   console.log("Request:", req.method, req.url, req.ip);
   next();
 }
