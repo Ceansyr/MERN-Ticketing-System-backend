d41b657b3a975e7b8175a8768be6fc8745b2f381
Ceansyr <chinmaymandavkar@gmail.com>
2025-05-02 15:21:49 +0530

    fixed team member editing route error

--- routes/team.js
+++ routes/team.js
@@ -8,6 +8,7 @@ applyCommonMiddleware(router);
 
 router.get("/members", TeamController.getTeamMembers);
 router.post("/invite", TeamController.inviteTeamMember);
+router.put("/members/:memberId", TeamController.updateTeamMember);
 router.delete("/members/:memberId", TeamController.removeTeamMember);
 router.get("/invitations", TeamController.getPendingInvitations);
 router.get("/invitations/:email", TeamController.checkInvitation);
