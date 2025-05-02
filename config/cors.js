60d4ec4e8eaf76f45d7ae268428a37c5e651c3ea
Ceansyr <chinmaymandavkar@gmail.com>
2025-05-02 13:56:51 +0530

    added deployed frontend link to cors

--- config/cors.js
+++ config/cors.js
@@ -1,12 +1,12 @@
 export const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://mern-ticketing-system-frontend.vercel.app" || "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 };
 
 export const corsMiddleware = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://mern-ticketing-system-frontend.vercel.app" || "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
